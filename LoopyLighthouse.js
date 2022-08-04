b2ba274e34d1b4b189f3ce35c20f09f0d7d8e686
Travis <travisliu708@gmail.com>
2022-08-03 21:33:40 -0700

    Including loopyligthhouse file, used to understand loops

---
+++ LoopyLighthouse.js
@@ -0,0 +1,15 @@
+let i = 100;
+
+while (i <= 200) {
+
+  if (i % 3 === 0 && i % 4 === 0){
+    console.log("LoopyLighthouse");
+  } else if (i % 4 === 0){
+    console.log("Lighthouse");
+  } else if (i % 3 === 0){
+    console.log("Loopy")
+  } else {
+    console.log(i);
+  }
+  i++
+}
